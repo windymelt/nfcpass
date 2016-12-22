8461325ea6a37c4d60cdd7be0d75aa144b17a99c
Windymelt <qwilas@3qe.us>
2016-12-22 21:40:15 +0900

    use library

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import nfc
-from Crypto.Cipher import AES
+from simple_aes_cipher import AESCipher, generate_secret_key
 import array
 import sys
+import base64
 
 def connected(tag):
     keys = [tag.read(page) for page in range(0x04, 0x06)]
     combined_key = array.array('B', [item for sl in keys for item in sl]).tostring()
-    obj = AES.new(combined_key, AES.MODE_CBC, 'This is iv123456')
-    ciphertext = obj.encrypt('secret data which is padded with 16 bytes.......')
-    print(ciphertext)
+    obj = AESCipher(combined_key)
+    #ciphertext = obj.encrypt('secret data')
+    #b64ciphertext = base64.b64encode(ciphertext)
+    #print(b64ciphertext)
+    with open('secret.base64', 'r') as f:
+        print(obj.decrypt(base64.b64decode(f.readline())))
     sys.exit(0)
     return True
 
